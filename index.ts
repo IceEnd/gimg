1ca11bb616ac0da3a70ba3d0e43d45021203e2e9
IceEnd <min@coolecho.net>
2018-09-17 20:55:41 +0800

    update

--- index.ts
+++ index.ts
@@ -4,5 +4,30 @@ import program from 'commander';
 import PKG from './package.json';
 
 program
-  .version(PKG.version)
+  .version(PKG.version);
+
+program
+  .command('create <name>')
+  .description('Generate image')
+  .usage('[options] <file ...>')
+  .option('-s, --size <size>', 'Image size', /^d+xd+$/i, '500x500')
+  .option('-t, --text <text>', 'Text')
+  .option('-o, --out <path>', 'Out file path')
+  .action((...args) => {
+    console.log(args);
+  });
+
+program
+  .command('help')
+  .description('Print this help')
+  .action(() => {
+      program.outputHelp();
+  })
   .parse(process.argv);
+
+program
+  .parse(process.argv);
+
+if (process.argv.length === 2) {
+  program.outputHelp();
+}
